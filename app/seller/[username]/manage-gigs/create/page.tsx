06676418f4f02ab37b506f0a7db16fe69a44e3c8
dbootable@gmail.com <dbootable@gmail.com>
2024-07-25 10:37:51 +0100

    schema

---
+++ app/seller/[username]/manage-gigs/create/page.tsx
@@ -0,0 +1,19 @@
+import { CreateForm } from "../../../[username]/manage-gigs/create/_components/create-form";
+
+interface CreateGigProps {
+    params: {
+        username: string;
+    }
+}
+
+const CreateGig = ({
+    params
+}: CreateGigProps) =>{
+    return (
+        <div className="flex justify-center">
+           <CreateForm username={params.username} />
+        </div>
+    )
+}
+
+export default CreateGig
