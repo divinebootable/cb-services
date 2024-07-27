3d5d6d991ad5039473f039ff2ab9e22a5d725c59
dbootable@gmail.com <dbootable@gmail.com>
2024-07-27 13:16:59 +0100

    gig edit backend functions: remove , updateDescription , update, favorite, unfavorite, getSeller, getCategoryAndSubcategory

--- convex/gig.ts
+++ convex/gig.ts
@@ -164,3 +164,230 @@ export const unpublish = mutation({
       return gig;
   },
 });
+
+export const remove = mutation({
+  args: { id: v.id("gigs") },
+  handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+
+      if (!identity) {
+          throw new Error("Unauthorized");
+      }
+
+      const user = await ctx.db
+          .query("users")
+          .withIndex("by_token", (q) =>
+              q.eq("tokenIdentifier", identity.tokenIdentifier)
+          )
+          .unique();
+
+      if (user === null) {
+          return;
+      }
+
+      //const userId = identity.subject as Id<"users">;
+      const userId = user._id;
+      const existingFavorite = await ctx.db
+          .query("userFavorites")
+          .withIndex("by_user_gig", (q) =>
+              q
+                  .eq("userId", userId)
+                  .eq("gigId", args.id)
+          )
+          .unique();
+
+      if (existingFavorite) {
+          await ctx.db.delete(existingFavorite._id);
+      }
+
+      await ctx.db.delete(args.id);
+  },
+});
+
+export const updateDescription = mutation({
+  args: { id: v.id("gigs"), description: v.string() },
+  handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+
+      if (!identity) {
+          throw new Error("Unauthorized");
+      }
+
+      const description = args.description.trim();
+
+      if (!description) {
+          throw new Error("Description is required");
+      }
+
+      if (description.length > 20000) {
+          throw new Error("Description is too long!")
+      }
+
+      const gig = await ctx.db.patch(args.id, {
+          description: args.description,
+      });
+
+      return gig;
+  },
+});
+
+export const update = mutation({
+  args: { id: v.id("gigs"), title: v.string() },
+  handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+
+      if (!identity) {
+          throw new Error("Unauthorized");
+      }
+
+      const title = args.title.trim();
+
+      if (!title) {
+          throw new Error("Title is required");
+      }
+
+      if (title.length > 60) {
+          throw new Error("Title cannot be longer than 60 characters")
+      }
+
+      const gig = await ctx.db.patch(args.id, {
+          title: args.title,
+      });
+
+      return gig;
+  },
+});
+
+export const favorite = mutation({
+  args: { id: v.id("gigs") },
+  handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+
+      if (!identity) {
+          throw new Error("Unauthorized");
+      }
+
+      const gig = await ctx.db.get(args.id);
+
+      if (!gig) {
+          throw new Error("Board not found");
+      }
+
+      const user = await ctx.db
+          .query("users")
+          .withIndex("by_token", (q) =>
+              q.eq("tokenIdentifier", identity.tokenIdentifier)
+          )
+          .unique();
+
+      if (user === null) {
+          return;
+      }
+
+      const userId = user._id;
+
+      const existingFavorite = await ctx.db
+          .query("userFavorites")
+          .withIndex("by_user_gig", (q) =>
+              q
+                  .eq("userId", userId)
+                  .eq("gigId", gig._id)
+          )
+          .unique();
+
+      if (existingFavorite) {
+          throw new Error("Board already favorited");
+      }
+
+      await ctx.db.insert("userFavorites", {
+          userId,
+          gigId: gig._id,
+      });
+
+      return gig;
+  },
+});
+
+export const unfavorite = mutation({
+  args: { id: v.id("gigs") },
+  handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+
+      if (!identity) {
+          throw new Error("Unauthorized");
+      }
+
+      const gig = await ctx.db.get(args.id);
+
+      if (!gig) {
+          throw new Error("Board not found");
+      }
+
+      const user = await ctx.db
+          .query("users")
+          .withIndex("by_token", (q) =>
+              q.eq("tokenIdentifier", identity.tokenIdentifier)
+          )
+          .unique();
+
+      if (user === null) {
+          return;
+      }
+
+      const userId = user._id;
+
+      const existingFavorite = await ctx.db
+          .query("userFavorites")
+          .withIndex("by_user_gig", (q) =>
+              q
+                  .eq("userId", userId)
+                  .eq("gigId", gig._id)
+          )
+          .unique();
+
+      if (!existingFavorite) {
+          throw new Error("Favorited gig not found");
+      }
+
+      await ctx.db.delete(existingFavorite._id);
+
+      return gig;
+  },
+});
+
+export const getSeller = query({
+  args: { id: v.id("users") },
+  handler: async (ctx, args) => {
+      const seller = ctx.db.get(args.id);
+      return seller;
+  },
+});
+
+export const getCategoryAndSubcategory = query({
+  args: {
+      gigId: v.id("gigs"),
+  },
+  handler: async (ctx, args) => {
+      const gig = await ctx.db.get(args.gigId);
+
+      if (!gig) {
+          throw new Error("Gig not found");
+      }
+
+      const subcategory = await ctx.db.get(gig.subcategoryId);
+
+      if (!subcategory) {
+          throw new Error("Subcategory not found");
+      }
+
+      const category = await ctx.db.get(subcategory.categoryId);
+      if (!category) {
+          throw new Error("Category not found");
+      }
+
+      return {
+          category: category.name,
+          subcategory: subcategory.name,
+      };
+  }
+});
